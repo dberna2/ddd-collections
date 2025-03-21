319c9d4a2eb945f13d427f621e540538638cd7f1
David Bernal <leo.bernal1946@gmail.com>
2025-03-21 17:26:39 +0100

    chore: first commit

---
+++ src/main/java/com/dberna2/ddd/collections/DddCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.dberna2.ddd.collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddCollectionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DddCollectionsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dberna2/ddd/collections/DddCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dberna2.ddd.collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddCollectionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
